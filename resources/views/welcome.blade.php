94f0f844a9b47b0249a473906ff1a89cc8615cff
Muhammad Aqil <maqil2770@gmail.com>
2023-06-10 16:28:27 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        @livewireStyles
+    </head>
+    <body>
+       
+        @livewire('main-page')
+        @livewireScripts
+    </body>
+</html>
